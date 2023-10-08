5994e4004303d1fce011de92d94d628bc4377da2
Daniel Bovensiepen <oss@bovi.li>
2023-10-08 22:09:52 +0800

    fix: command call

--- src/daniel.bovensiepen.li/scripts/dotfiles.sh
+++ src/daniel.bovensiepen.li/scripts/dotfiles.sh
@@ -2,6 +2,6 @@
 
 cd && \
   GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" \
-  git clone git@github.com:bovi/stuff.git'
+  git clone git@github.com:bovi/stuff.git
 
 cd ~/stuff && ruby deploy_configs.rb
